366c8b78443a4159bd435fa3a15273ae9beb6ecc
japolinar <japolinar_1@hotmail.com>
2026-03-29 16:42:58 -0300

    Modificando ruta 15.1-archivos1

--- Udemy/15.1-archivos1.py
+++ Udemy/15.1-archivos1.py
@@ -2,11 +2,15 @@
 
 def app():
     #creando un archivo
-    archivo = open('archivo.txt', 'w') # w es escritura, si no existe lo creara 
+    archivo = open('Udemy/archivo.txt', 'w') # w es escritura, si no existe lo creara 
 
     #Generar numero en archivo
-    for i in range(1, 11):
+    for i in range(1, 16):
         archivo.write('Esta es la linea: ' + str(i) +'\r' )
+
+    print('\n-------------------------------')
+    print('Archivo Creado correctamente!!!')
+    print('-------------------------------')
 
     #Cerrando el archivo
     archivo.close()
